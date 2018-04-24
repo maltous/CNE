8c94f465ee8cb65186f89853fec910c672c455e1
maltous <35376350+maltous@users.noreply.github.com>
2018-04-24 08:26:17 +0300

    CNE
    CNE

---
+++ story_content/user.js
@@ -0,0 +1,195 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6VgLYEW9H78":
+        Script1();
+        break;
+      case "5xLWASrS8OO":
+        Script2();
+        break;
+      case "6kvw9RBnYnN":
+        Script3();
+        break;
+      case "6ENPQl3NlpX":
+        Script4();
+        break;
+      case "68W71vMbPgA":
+        Script5();
+        break;
+      case "5bcmdPiBBRH":
+        Script6();
+        break;
+      case "6jNmWsAU5Jq":
+        Script7();
+        break;
+      case "6DQQ5U6VU7U":
+        Script8();
+        break;
+      case "5deVwwjTD0w":
+        Script9();
+        break;
+      case "60m7vwVjopP":
+        Script10();
+        break;
+      case "69lN502FuaV":
+        Script11();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+var email="nsdd_cne@ksmc.med.sa";
+var subject=player.GetVar("Name");
+var body_start="Dear Instructor,";
+var mailto_link='mailto:'+email+'?subject='+subject+'&body='+body_start;
+win=window.open(mailto_link,'emailWin');
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+var email="nsdd_cne@ksmc.med.sa";
+var subject=player.GetVar("Name");
+var body_start="Dear Instructor,";
+var mailto_link='mailto:'+email+'?subject='+subject+'&body='+body_start;
+win=window.open(mailto_link,'emailWin');
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+var email="nsdd_cne@ksmc.med.sa";
+var subject=player.GetVar("Name");
+var body_start="Dear Instructor,";
+var mailto_link='mailto:'+email+'?subject='+subject+'&body='+body_start;
+win=window.open(mailto_link,'emailWin');
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+var email="nsdd_cne@ksmc.med.sa";
+var subject=player.GetVar("Name");
+var body_start="Dear Instructor,";
+var mailto_link='mailto:'+email+'?subject='+subject+'&body='+body_start;
+win=window.open(mailto_link,'emailWin');
+}
+
+function Script5()
+{
+  myWindow = window.open("","Print","width=810,height=610,scrollbars=1,resizable=1");
+var player=GetPlayer();
+var exercisenotes1="This acknowledges that";
+var exercisenotes2=player.GetVar("Name");
+var exercisenotes3="Completed ANNUAL CONTINUOUS NURSING EDUCATION PROGRAM successfully";
+var exercisenotes4="Which was conducted online at King Saud Medical City, Riyadh, Saudi Arabia";
+var exercisenotes5="Nurse Educator (sig.):__________________";
+var contents = "<html><head><title>Nursing Staff Development Department</title></head><body style='width:650px;padding:20px;'>"
+contents+="<div style='font-size:36px;font-weight:bold;text-align:center;margin-top:20px;'>CERTIFICATE OF COMPLETION</div>";
+contents+="<div style='font-size:22px;text-align:center;margin-top:30px;word-wrap:break-word'>"+exercisenotes1+"</div>";
+contents+="</body></html>"
+contents+="<div style='font-size:28px;font-weight:bold;text-align:center;margin-top:30px;word-wrap:break-word'>"+exercisenotes2+"</div>";
+contents+="</body></html>"
+contents+="<div style='font-size:28px;text-align:center;margin-top:30px;word-wrap:break-word'>"+exercisenotes3+"</div>";
+contents+="</body></html>"
+contents+="<div style='font-size:18px;text-align:center;margin-top:30px;word-wrap:break-word'>"+exercisenotes4+"</div>";
+contents+="</body></html>"
+contents+="<div style='font-size:18px;text-align:right;margin-top:150px;word-wrap:break-word'>"+exercisenotes5+"</div>";
+contents+="</body></html>"
+myWindow.document.write(contents);
+myWindow.document.close();
+myWindow.focus();
+myWindow.print();
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+var email="nsdd_cne@ksmc.med.sa";
+var subject=player.GetVar("Name");
+var body_start="Dear Instructor,";
+var mailto_link='mailto:'+email+'?subject='+subject+'&body='+body_start;
+win=window.open(mailto_link,'emailWin');
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+var email="nsdd_cne@ksmc.med.sa";
+var subject=player.GetVar("Name");
+var body_start="Dear Instructor,";
+var mailto_link='mailto:'+email+'?subject='+subject+'&body='+body_start;
+win=window.open(mailto_link,'emailWin');
+}
+
+function Script8()
+{
+  var player = GetPlayer();
+var email="nsdd_cne@ksmc.med.sa";
+var subject=player.GetVar("Name");
+var body_start="Dear Instructor,";
+var mailto_link='mailto:'+email+'?subject='+subject+'&body='+body_start;
+win=window.open(mailto_link,'emailWin');
+}
+
+function Script9()
+{
+  myWindow = window.open("","Print","width=810,height=610,scrollbars=1,resizable=1");
+var player=GetPlayer();
+var exercisenotes1="This acknowledges that";
+var exercisenotes2=player.GetVar("Name");
+var exercisenotes3="Completed AGE SPECIF CARE TRAINING and he/she due for exam";
+var exercisenotes4="Nurse Educator (sig.):__________________";
+var contents = "<html><head><title>Nursing Staff Development Department</title></head><body style='width:650px;padding:20px;'>"
+contents+="<div style='font-size:36px;font-weight:bold;text-align:center;margin-top:20px;'>EXAM ENTRY PERMISSION</div>";
+contents+="<div style='font-size:22px;text-align:center;margin-top:30px;word-wrap:break-word'>"+exercisenotes1+"</div>";
+contents+="</body></html>"
+contents+="<div style='font-size:28px;font-weight:bold;text-align:center;margin-top:30px;word-wrap:break-word'>"+exercisenotes2+"</div>";
+contents+="</body></html>"
+contents+="<div style='font-size:28px;text-align:center;margin-top:30px;word-wrap:break-word'>"+exercisenotes3+"</div>";
+contents+="</body></html>"
+contents+="<div style='font-size:18px;text-align:right;margin-top:150px;word-wrap:break-word'>"+exercisenotes4+"</div>";
+contents+="</body></html>"
+myWindow.document.write(contents);
+myWindow.document.close();
+myWindow.focus();
+myWindow.print();
+}
+
+function Script10()
+{
+  var player = GetPlayer();
+var email="nsdd_cne@ksmc.med.sa";
+var subject=player.GetVar("Name");
+var body_start="Dear Instructor,";
+var mailto_link='mailto:'+email+'?subject='+subject+'&body='+body_start;
+win=window.open(mailto_link,'emailWin');
+}
+
+function Script11()
+{
+  myWindow = window.open("","Print","width=810,height=610,scrollbars=1,resizable=1");
+var player=GetPlayer();
+var exercisenotes1="This acknowledges that";
+var exercisenotes2=player.GetVar("Name");
+var exercisenotes3="Completed END OF LIFE CARE TRAINING and he/she due for exam";
+var exercisenotes4="Nurse Educator (sig.):__________________";
+var contents = "<html><head><title>Nursing Staff Development Department</title></head><body style='width:650px;padding:20px;'>"
+contents+="<div style='font-size:36px;font-weight:bold;text-align:center;margin-top:20px;'>EXAM ENTRY PERMISSION</div>";
+contents+="<div style='font-size:22px;text-align:center;margin-top:30px;word-wrap:break-word'>"+exercisenotes1+"</div>";
+contents+="</body></html>"
+contents+="<div style='font-size:28px;font-weight:bold;text-align:center;margin-top:30px;word-wrap:break-word'>"+exercisenotes2+"</div>";
+contents+="</body></html>"
+contents+="<div style='font-size:28px;text-align:center;margin-top:30px;word-wrap:break-word'>"+exercisenotes3+"</div>";
+contents+="</body></html>"
+contents+="<div style='font-size:18px;text-align:right;margin-top:150px;word-wrap:break-word'>"+exercisenotes4+"</div>";
+contents+="</body></html>"
+myWindow.document.write(contents);
+myWindow.document.close();
+myWindow.focus();
+myWindow.print();
+}
+
